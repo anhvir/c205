992f2b355c324e9a22d8ba336c47dc1fbb9db482
anhvir <anhvir@gmail.com>
2019-04-28 09:37:45 +1000

    Week 8

--- README.md
+++ README.md
@@ -1,55 +1,25 @@
- c205 Week 07:
+c205 Week 08:
 =======
 
-Today's task: Learn how to submit your assignment. You should build a 
-very simple program and make sure that you can submit it. If possible,
-finish Stage 1 today.
+For this week, main topics include structs and arrays of structs. 
 
-Additional files:
----------------- 
-
-  * `ass1_notes.pdf` : 
+ass1_notes.pdf
+--------------
 Notes and advices on assignments 1, including the submission process.
+(Same as last week).
 
-  * `array.c` :
-Simple examples on using arrays, can be used as skeleton for exercises 7.3 and 7.4.
+int_sort.c  and array_int.txt
+-----------------------------
+sample and skeleton program and data for sorting int arrays (insertion, selection, recursive)
 
-  * `array_int.txt` : a data file of 20 integers
+anagram_e7.15.c
+---------------
+a skeleton for writing string function such as ex 7.15, ex 7.14, ex 7.12 (palindrome)
 
-  * `structs.c and students.txt`: **Warning:** This is an example of using
-structs and arrays of structs. You do **not** need to use structs 
-to complete your assignment, **no rewards** for attempting structs.
-However, if you use, your program will be simpler and better. If
-you think you want to use structs, you can read the textbook
-chapter on structs, and have a look at `structs.c`.
+ass_notes.pdf: same as last week, advices for assignment 1.
 
-`structs.c` a simple program and data for reading and storing a list of student records.
-For simplicity, each student record has only 3 component: student ID, ATAR score, and year of entering the university.
-
-After compiling, you can run the program with:
-
-`./structs < students.txt`
-
-in this case the program will run with data read from the file `students.txt` instead of from the keyboard. You also can do:
-
-`./structs < students.txt > structs.out`
-
-in this case, the output will be written to the file `structs.out` instead of to the computer's display.
-
-
-------------------------------------------------------
-HOWTO download this repository to your computer:
-
-If you just want a single file, say, `prog.c`:
-  * Click on `prog.c` and you will see its content 
-  * Copy the content and paste into your jEdit/editor window, then save it in your working folder
-
-If you want to download the whole directory:
-  * Download this whole directory by clicking `Clone or Download` --> `Download ZIP`, it will normally be downloaded to your `Downloads` folder under the name `c205-master` or `c205-master.ZIP`
-  * Then copy or move the folder `c205-master` from `Downloads` to under your working folder
-  * If the downloaded `c205-master` has the extension `.ZIP`, you likely need to expand the zipped file with command `unzip c205-master.ZIP`. If the command doesn't work, ask Google how to unzip.
- 
 -------------------------------------------------------------
+
 by anhvir@gmail.com, for use in unimelb COMP20005 workshops.
 
 For a teaching week, a new content is (normally) uploaded at the very beginning of the week, and that content is removed one week later.
--- int_sort.c
+++ int_sort.c
@@ -1,92 +1,56 @@
 /*    
-	Purpose : using array (with random values) 
+	Purpose : Skeleton for sorting arrays of integers 
    
-	Skeleton by: Anh Vo, anhvir@gmail.com, 05 Apr 2017
-	Finished by:
+	Started by: Anh Vo, anhvir@gmail.com
+	Finished by: 
 
-	Instructions:
-		- compile and try the program
-		- implement points ****1**** and *****2*****  
-		- implement point ****3**** if you can
-
-    Note: NO CHANGE from last week
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 20
+#define MAX_N 1000		/* maximal array size */ 
 
 int readArray(int a[]); 
 void printArray(char *title, int a[], int n);
-int sum(int a[], int n); 
 void ins_sort(int a[], int n);
+void sel_sort(int a[], int n);
+void rec_sel_sort(int a[], int n);
+void rec_ins_sort(int a[], int n);
 
 int
 main(int argc, char *argv[]) {
-	int a[N];
-	int n;
+	int n=0;			/* current number of elements in array */
+	int a[MAX_N];
 
 	n= readArray(a);
 	printArray("Original array", a, n);
 
-	/* ****1**** */
-    /* squareArray(a, N); */    
-    /* YOU HAVE TO WRITE function squareArray
-		that chang each element of array to
-		its squared value, and call it here   */
-	printArray("New array", a, n);
-
-    printf("sum of array elements= %d\n", sum(a, n));
-
-    printf("\n SORTING:\n");
+	/* change the next 2 lines to switch to the appropriated sorting */	
 	ins_sort(a,n);
-	printArray("\nSorted array:", a, n);
-
-
-	/* ****2**** */
-	/* printf("sum of even elements of array= %d\n", sumEven(A, n); */
-	/* YOU HAVE TO WRITE function sumEven that returns the sum of 
-		the elements which are even */
-	
-	/* ****3****: write and call a function to sort the array
-					in increasing order  */ 
+	printArray("Insertion Sort. Sorted array= ", a, n);
 
 	return 0;
 }
-
 
 /* reads array from stdin  
    returns the number of elements in the array
 */ 
 int readArray(int a[]) {  
 	int i;
-	for (i=0; i<N && scanf("%d", &a[i])==1; i++);  /* empty body */
+	for (i=0; i<MAX_N && scanf("%d", &a[i])==1; i++);  /* empty body */
 	return i;
 }
 
 
-/* print out the array after printing a title,
-   Note that here "int *a" is another way for writing "int a[]"
+/* print out the array a[n] after printing a title
 */  
 void printArray(char *title, int a[], int n) {
 	int i;
-	printf("%s: ", title);
+	printf("%s:\n", title);
 	for (i=0; i<n; i++) printf ("%3d", a[i]);
 	printf("\n\n");
 }
-
-/* returns sum od elements of arrays a */
-int sum(int a[], int n) {
-	int i, sum=0;
-	for (i=0; i<n; i++) {
-		sum += a[i];
-	}
-	return sum;
-}
-
-
-
 
 
 /* swapping values of two integer locations
@@ -121,3 +85,28 @@ void ins_sort(int a[], int n){
 	}
 }
 
+/* sorting elements of a[] in increasing order
+   using selection sort, see ex 7.6     
+*/	
+void sel_sort(int a[], int n){
+
+}
+
+
+/* sorting elements of a[] in increasing order
+   using recursive selection sort 
+
+   Logic: to sort n elements of a[]:
+		- first, arrange the largest to the last position
+		- then, sort n-1 elements of a[], using the same method   
+	   
+*/	
+void rec_sel_sort(int a[], int n){
+ 
+}
+
+
+/* recursive insertion sort */
+void rec_ins_sort(int a[], int n){
+ 
+}
