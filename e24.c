843a0cbf4da31f3c1c8c3d586e74dd1cae459801
anhvir <anhvir@gmail.com>
2020-03-08 19:02:47 +1100

    Week 2

---
+++ e24.c
@@ -0,0 +1,19 @@
+/*
+  Program skeleton for exercise 2.4
+  - print out the value of 
+	 INT_MIN, INT_MAX
+	 FLT_MIN, FLT_MAX
+	 DBL_MIN, DBL_MAX
+  
+*/
+
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int
+main(int argc, char *argv[]) {
+	/* print out the six predefined values */
+
+	return 0;
+}
