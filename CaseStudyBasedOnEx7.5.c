3fc378560a41ede9a1063569695f2bdd29130bc9
anhvir <anhvir@gmail.com>
2020-04-27 11:42:10 +1000

    Week 7

---
+++ CaseStudyBasedOnEx7.5.c
@@ -0,0 +1,29 @@
+/* Ex 7.5
+Suppose that a set of "student number, mark" pairs are provided,
+one pair of numbers per line, with the lines in no particular order.
+Write a program that reads this data and outputs the same data, 
+but ordered by student number. For example:
+
+823678  66
+765876  94
+864876  48
+785671  68
+854565  89
+On this input your program should output:
+
+Enter as many as 1000 "studnum mark" pairs, ^D to end
+5 pairs read into arrays
+studnum  mark
+765876  94
+785671  68
+823678  66
+854565  89
+864876  48
+
+------------------------
+In this Case Study:
+  + using typedef
+  + using struct
+  + recall: using selection sort
+  + recall: usng redirection
+------------------------------------------*/
---
+++ ex7.4.c
@@ -0,0 +1,25 @@
+/* Exercise 7.4  ---------------------------------- 
+Write a program that reads as many as 1,000 integer values,
+and counts the frequency of each value in the input:
+
+ 
+./program
+Enter as many as 1000 values, ^D to end
+1 3 4 6 4 3 6 10 3 5 4 3 1 6 4 3 1
+17 values read into array
+Value Freq
+   1    3
+   3    5
+   4    4
+   5    1
+   6    3
+  10    1
+
+--- Topics covered:
+  + coventional processing arrays: using functions
+  + REDIRECTION: input data from a text file (instead of keyboard) to array
+  + SELECTION SORT (recursive version)
+  + REDIRECTION: output to a file (instead of screen)
+
+------------------------------------------------*/
+
